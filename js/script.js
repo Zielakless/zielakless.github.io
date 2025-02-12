f3e89fb23b99303dab9312b19d712a640c40ef48
Zielakless <Zielakless@gmail.com>
2025-02-12 21:57:36 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 var backToTopButton = document.getElementById(`backToTop`);
 
 window.onscroll = () => backToTopButton.style.display = (document.documentElement.scrollTop > 1) ? `flex` : `none`;
+
+function musicPlayer() {
+    var musicPlayer = document.getElementById(`musicPlayer`);
+
+    if(!musicPlayer.paused) {
+        musicPlayer.pause();
+        musicIcon.className = "fa-solid fa-play";
+    } else {
+        musicPlayer.play();
+        musicIcon.className = "fa-solid fa-pause";
+    }
+}
+
 backToTop = () => document.documentElement.scrollTop = 0;
